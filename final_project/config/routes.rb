09da4716c00d6a91648a786087d571be056adaff
beatrizSO <bia.santosdoliveira@gmail.com>
2020-06-30 20:16:06 -0300

    homepage funcionando

--- final_project/config/routes.rb
+++ final_project/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root 'application#homepage'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, :products, :orders
+  
 end
