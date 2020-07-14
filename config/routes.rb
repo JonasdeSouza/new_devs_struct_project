c540c68d9e9ccc7c0a8cc9d86244ce04c2491975
David Mendes <davidgmendes@hotmail.com>
2020-07-14 05:26:14 -0300

    Views dos Usuários, Produtos e Pedidos diferentes para adms, usuarios comuns e usuarios nao logados

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   root 'application#homepage'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, :products, :orders, :addresses
+  resources :users, :addresses
+  resources :products, :orders
   post "/checkout", to: "orders#checkout", as: :checkout
 
   #Rotas para as páginas de depositar e retirar dinheiro da conta
